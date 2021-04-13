43754ecb6a877e1be4f1c482696127ba29a07c11
Jorge <78658812+jandres54@users.noreply.github.com>
2021-04-13 00:37:21 -0500

    commit day 1

--- frontEnd.js
+++ frontEnd.js
@@ -1,9 +1,19 @@
-const search = () => {
-    let zip = document.querySelector(".search-bar").value;
-    console.log(zip);
-    fetch(` https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=E85,ELEC&state=CA&limit=2&api_key=fS0wmzxjg7aqScd1ol16imIA5cLVIBPtenOqTXHZ`)
+let button1 = document.querySelector("#energy");
 
-    const btn = document.querySelector(".button");
-    btn.addEventListener("click", getweather);
+const body = document.querySelector(".root");
 
+const getFuel = async () => {
+  const energy = await fetch("https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=E85,ELEC&state=CA&limit=2&api_key=fS0wmzxjg7aqScd1ol16imIA5cLVIBPtenOqTXHZ");
+
+  const convertedJson = await energy.json();
+
+  console.log(convertedJson);
+
+  const stationsInfo = document.createElement("p");
+
+    console.log(stationsInfo);
 }
+button1.addEventListener("click", () => {
+    getFuel();
+  });
+  
