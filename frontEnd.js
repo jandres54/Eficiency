38de1c3aaab9cbbe581c335d57d7d264c54d81ea
Jorge <78658812+jandres54@users.noreply.github.com>
2021-04-14 14:47:37 -0500

    progress 04/14

--- frontEnd.js
+++ frontEnd.js
@@ -7,25 +7,41 @@ const getFuel = async () => {
 
   const convertedJson = await energy.json();
 
+  
+
+let getName = convertedJson.fuel_stations[0].station_name 
+
+let getFuel = convertedJson.fuel_stations[0].fuel_type_code
+
+let getPhone = convertedJson.fuel_stations[0].station_phone
+
+let getAddress = convertedJson.fuel_stations[0].street_address
+
+
   console.log(convertedJson);
 
-  console.log(convertedJson.fuel_stations[0].station_name);
 
-  console.log(convertedJson.fuel_stations[0].fuel_type_code);
+const info = ("Store name: " + getName + " " + "Fuel type: " + getFuel + " "+ "Phone number: " +getPhone + " " + "Store address: " + getAddress)
+console.log(info)
 
+//target the container div 
+const container = document.querySelector(".stationcontainer")
 
+//create the ul element
+const uList = document.createElement("ul")
+//create the list element
+const list = document.createElement("li")
+//append ul 
+container.append(uList)
+//append li
+uList.append(list)
 
+//append info
+list.append(info)
 
-
-  const stationsInfo = document.createElement("p");
-
-    console.log(stationsInfo);
 }
 
 
 button1.addEventListener("click", getFuel)
 
 
-
-
-  
